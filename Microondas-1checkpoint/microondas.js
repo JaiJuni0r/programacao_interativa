2784daef7f59cffde1370df82f870965da630ae4
Jailson <jailson_junior123@hotmail.com>
2022-03-02 03:25:44 -0300

    Checkpoint de programação interativa!

--- Microondas-1checkpoint/microondas.js
+++ Microondas-1checkpoint/microondas.js
@@ -1,98 +1,103 @@
-// Olá, você foi contratado para executar este projeto. É importante que você analise, entenda o pedido do cliente e desenvolva conforme solicitado. Veja abaixo os requisitos do projeto:
-
-// - Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
-
 //       1 - Pipoca – 10 segundos (padrão);
 //       2 - Macarrão – 8 segundos (padrão);
 //       3 - Carne – 15 segundos (padrão);
 //       4 - Feijão – 12 segundos (padrão);
-//       5 - Brigadeiro – 8 segundos (padrão); 
-
-// - O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-// - Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
-// - Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
-// - Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-// - No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
-
-console.log(`
-1 - Pipoca – 10 segundos (padrão);
-2 - Macarrão – 8 segundos (padrão);
-3 - Carne – 15 segundos (padrão);
-4 - Feijão – 12 segundos (padrão);
-5 - Brigadeiro – 8 segundos (padrão); 
-    ESCOLHA SEU PRATO!!
-`)
+//       5 - Brigadeiro – 8 segundos (padrão);
 
 
+//Por favor, coloque a primeira letra como maiuscula!
 
-let prato =  "Bolo"
-let tempo = "12s"
-console.log(prato,tempo)
+let prato = "Brigadeiro"
 
-switch(tempo){
+//Por favor, defina o tempo de forno:
+var tempo = 24
 
-    //PIPOCA
 
-        case 1, "10s":
-            console.log( "Prato pronto, bom apetite!!!");
-            break
-        case 2, "20s":
-            console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
-            break
-        case 3, "30s":
-            console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
-            break
+switch (prato) {
+
+    //Pipoca
+    case "Pipoca":
+        console.log("Sua escolha foi: Pipoca")
+        if(tempo <=9){
+            console.log("Porém, o tempo insuficiente!")
+        } 
+        else if(tempo >= 10 && tempo <= 19){
+            console.log("Seu prato está pronto!")
+        } 
+        else if(tempo >=20 && tempo <= 29){
+            console.log("Seu prato queimou!")
+        } 
+        else if(tempo >= 30)
+            console.log("KABUUUM!!!")
+
+        break;
+
+    //Macarrão
+    case "Macarrão":
+        console.log("Sua escolha foi: Macarrão")
+        if(tempo <=7){
+            console.log("Porém, o tempo insuficiente!")
+        } 
+        else if(tempo >= 8 && tempo <= 15){
+            console.log("Seu prato está pronto!")
+        } 
+        else if(tempo >=16 && tempo <= 23){
+            console.log("Porém, seu prato queimou!")
+        } 
+        else if(tempo >=24)
+            console.log("KABUUUM!!!")
+        break;
+    //Carne
+    case "Carne":
+            console.log("Sua escolha foi: Carne")
+            if(tempo <=14){
+                console.log("Porém, o tempo insuficiente!")
+            } 
+            else if(tempo >= 15 && tempo <= 29){
+                console.log("Seu prato está pronto!")
+            } 
+            else if(tempo >=30 && tempo <= 44){
+                console.log("Porém, seu prato queimou!")
+            } 
+            else if(tempo >=45)
+                console.log("KABUUUM!!!")
+            break;
+    //Feijão
+    case "Feijão":
+        console.log("Sua escolha foi: Feijão")
+        if(tempo <=11){
+            console.log("Porém, o tempo insuficiente!")
+        } 
+        else if(tempo >= 12 && tempo <= 23){
+            console.log("Porém, seu prato queimou!")
+        } 
+        else if(tempo >=24 && tempo <= 35){
+            console.log("Seu prato queimou!")
+        } 
+        else if(tempo >=36)
+            console.log("KABUUUM!!!")
+        break;
     
-   //MACARRÃO
-        case 4, "8s":
-              console.log( "Prato pronto, bom apetite!!!");
-                break
-        case 5, "16s":
-             console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
-                break
-        case 6, "24s":
-             console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
-                break
+    //Brigadeiro
+    case "Brigadeiro":
+        console.log("Sua escolha foi: Brigadeiro")
+        if(tempo <=7){
+            console.log("Tempo insuficiente")
+        } 
+        else if(tempo >= 8 && tempo <= 15){
+            console.log("Porém, seu prato queimou!")
+        } 
+        else if(tempo >=16 && tempo <= 23){
+            console.log("Seu prato queimou!")
+        } 
+        else if(tempo >=24)
+            console.log("KABUUUM!!!")
+        break;
 
-    //CARNE
-        case 7, "15s":
-              console.log( "Prato pronto, bom apetite!!!");
-                break
-        case 8, "30s":
-             console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
-                break
-        case 9, "45s":
-             console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
-                break
-
-
-       //FEIJÃO   
-
-        case 10, "12s":
-            console.log( "Prato pronto, bom apetite!!!");
-                    break
-        case 11, "24s":
-              console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
-                    break
-        case 12, "36s":
-             console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
-                    break
-
-    //BRIGADEIRO
-        case 13, "8s":
-             console.log( "Prato pronto, bom apetite!!!");
-                        break
-         case 14, "16s":
-             console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
-                        break
-         case 15, "24s":
-              console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
-                        break
-                        
-
-        default:(
-            console.log("Tempo insuficiente!!")
-    )    
-
-        
+    //Prato inexistente
+    default:
+        console.log("Prato inexistente.")    
 }
+
+
+console.log("Prato pronto, tenha um bom apetite!")
