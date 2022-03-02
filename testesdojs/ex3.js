b1839150ed72f88f095d45cee41ac6583a0426da
Jailson <jailson_junior123@hotmail.com>
2022-03-02 03:24:02 -0300

    Checkpoint de programação interativa!

---
+++ testesdojs/ex3.js
@@ -0,0 +1,28 @@
+var soma = 1 + 2;
+console.log(soma);
+
+var multip = 12 * 2;
+console.log(multip)
+
+console.log(soma+multip)
+
+var somageral = soma + multip
+console.log(somageral)
+
+//Ele ignora o "" para realizar o calculo
+somageral = "22" * 2
+console.log(somageral)
+
+//Se colocar o "+", ele identifica como concatenacão de string
+somageral = "22 " + 2
+console.log(somageral)
+
+somageral = "olá " + 23
+console.log(somageral)
+
+const nome = "Jai"
+const idade = 23
+
+somageral = "Meu nome é: " + nome + " e tenho " + idade + " anos."
+console.log(somageral)
+
---
+++ testesdojs/ex4.js
@@ -0,0 +1,54 @@
+/* Quais são as variaveis?
+Variaveis são:
+Nome, sobrenome, idade, peso, altura e plano */
+/* Quais são as constantes?
+As constantes são:
+Nome, sobrenome*/
+/* Qual variável poderia armazenar o valor null 
+segundo estes dados?
+A unica que pode é a:
+Plano*/
+/* const nome = "José"
+const sobrenome = "da Silva"
+const idade = 27 
+const peso = 83.5 
+const altura = 1.70 
+const plano = true
+const imc = peso/altura**2 */
+
+// Escopo da função
+ //bloco de codigos onde será executado
+ // sempre estando entre {}
+
+function nome_cliente(nome = ""){
+ 
+    console.log()
+}
+
+nome_cliente(Jailson)
+
+//como chamar a função
+
+function idade_clientes(idade= ""){
+    console.log()
+}
+
+idade_clientes(23)
+
+function altura_cliente(altura = ""){
+    console.log()
+}
+
+altura_cliente(1.88)
+
+function peso_cliente(peso = ""){
+    console.log()
+}
+
+peso_cliente(82.4)
+
+console.log("")
+
+const imc = altura_cliente * peso_cliente**2
+
+console.log("O " + nome + " tem " + idade)
